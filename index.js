bee376880822bcbf6fad1604ae22f5d2d74528ba
crescens tan <tancrescens@gmail.com>
2025-01-19 14:53:31 +0000

    create a new Express application

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get("/", (req, res)=>{
+    res.json({message: "welcome to the API"})
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
