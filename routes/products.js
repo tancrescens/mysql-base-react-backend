8d7606e14de601d8b9cfeee49a05086416e241d7
crescens tan <tancrescens@gmail.com>
2025-01-19 15:17:48 +0000

    Using services in the product router

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database');
+const productService = require('../services/productService');
 
 // GET all products
-router.get('/', (req, res) => {
-  res.json({ message: "Get all products" });
+router.get('/', async (req, res) => {
+  try {
+    const products = await productService.getAllProducts();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // GET a single product
-router.get('/:id', (req, res) => {
-  res.json({ message: `Get product with id ${req.params.id}` });
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 module.exports = router;
