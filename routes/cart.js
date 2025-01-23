9cfa986463d2145756156d6185f6d5e2aa4c7cbd
crescens tan <tancrescens@gmail.com>
2025-01-23 06:03:52 +0000

    update cart route for userAuth

--- routes/cart.js
+++ routes/cart.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const cartService = require('../services/cartService');
+const authenticateToken = require('../middleware/UserAuth');
+
+// Apply the authenticateToken middleware to all routes
+router.use(authenticateToken);
 
 // GET cart contents
 router.get('/', async (req, res) => {
-    try {
-        res.send("Get Cart Route");
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const cartContents = await cartService.getCartContents(req.user.userId);
+    res.json(cartContents);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // PUT bulk update cart
 router.put('/', async (req, res) => {
-    try {
-        res.send("Put Cart Route")
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const cartItems = req.body.cartItems; // Expects an array of items with productId and quantity
+    await cartService.updateCart(req.user.userId, cartItems);
+    res.json({ message: 'Cart updated successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
