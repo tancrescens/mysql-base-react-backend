34d00af8f8ff75efbc05667e6e2b491a598d9b14
crescens tan <tancrescens@gmail.com>
2025-01-24 06:25:57 +0000

    update cart

--- data/cartData.js
+++ data/cartData.js
@@ -1,13 +1,19 @@
 const pool = require('../database');
 
 async function getCartContents(userId) {
-  const [rows] = await pool.query(`
-        SELECT * FROM cart_items JOIN products
-          ON cart_items.product_id = products.id
+    const [rows] = await pool.query(`
+        SELECT c.id, 
+               c.product_id, 
+               p.image AS imageUrl, 
+               p.name AS productName,
+               CAST(price AS DOUBLE) AS price,
+               c.quantity 
+               FROM cart_items AS c JOIN products AS p
+          ON c.product_id = p.id
           WHERE user_id = ?
         `, [userId]);
 
-  return rows;
+    return rows;
 }
 
 // Data format
@@ -16,38 +22,38 @@ async function getCartContents(userId) {
 //  quantity: the number of the quantity
 //}]
 async function updateCart(userId, cartItems) {
-  const connection = await pool.getConnection();
-  try {
-    await connection.beginTransaction();
-
-    // 1. delete the old cart 
-    connection.query('DELETE FROM cart_items WHERE user_id = ?', [userId])
-
-    // 2. recreate the cart
-    // this is an inefficent solution but you can try
-    // creating an efficient solution where mutliple rows
-    // are inserted with one query
-    for (let cartItem of cartItems) {
-      await connection.query(`
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
+       
+        // 1. delete the old cart 
+        connection.query('DELETE FROM cart_items WHERE user_id = ?', [userId])
+       
+        // 2. recreate the cart
+        // this is an inefficent solution but you can try
+        // creating an efficient solution where mutliple rows
+        // are inserted with one query
+        for (let cartItem of cartItems) {
+            await connection.query(`
                 INSERT INTO cart_items(user_id, product_id, quantity)
                     VALUES (?,?,?)
                 
                 `, [userId, cartItem.product_id, cartItem.quantity])
+        }
+        
+        // finalize the changes to the database
+        await connection.commit();
+
+    } catch (e) {
+        // any error when caught we rollback the database
+        await connection.rollback();
+    } finally {
+        connection.release();
     }
-
-    // finalize the changes to the database
-    await connection.commit();
-
-  } catch (e) {
-    // any error when caught we rollback the database
-    await connection.rollback();
-  } finally {
-    connection.release();
-  }
 }
 
 
 module.exports = {
-  getCartContents,
-  updateCart
+    getCartContents,
+    updateCart
 }
